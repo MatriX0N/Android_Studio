72d16cb626669e15525d5663d78c2728066e0dcf
MatriX0N <stas5432167890@gmail.com>
2024-02-08 09:17:25 +0200

    first commit

---
+++ app/src/main/java/com/example/lb1/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.lb1
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myImageView: ImageView = findViewById(R.id.imageView)
+        val textV: TextView = findViewById(R.id.textView)
+        val intent = Intent(this,Second::class.java)
+        myImageView.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                textV.text="Kav"
+            }
+        })
+        textV.setOnClickListener {
+            if (textV.text == "Kav")
+                startActivity(intent)
+        }
+    }
+}
