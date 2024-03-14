ea5f0dcb6de18b0b737941705cfda5b8f4437f67
MatriX0N <stas5432167890@gmail.com>
2024-03-14 09:36:45 +0200

    main

---
+++ app/src/main/java/com/example/lb1/Shop.kt
@@ -0,0 +1,9 @@
+package com.example.lb1
+
+data class Shop(
+    val name: String,
+    val adddres: String,
+    val phoneNumber: String,
+    val open: String,
+    val close: String,
+)
---
+++ app/src/main/java/com/example/lb1/ShopAdapter.kt
@@ -0,0 +1,5 @@
+package com.example.lb1
+
+class ShopAdapter {
+
+}
